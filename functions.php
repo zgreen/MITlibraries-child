dd4ab1677fc1d24d54dcc20da3867659cfe7eada
Zach Green <zgreen@mit.edu>
2014-03-21 13:03:55 -0400

    Hook wp_enqueue_scripts

--- functions.php
+++ functions.php
@@ -1,12 +1,13 @@
 <?php
 
-add_action( 'after_setup_theme', 'remove_child_theme_support');
+add_action( 'wp_enqueue_scripts', 'libraries_child_scripts_styles');
 
 if (!function_exists('libraries_child_scripts_styles')) {
-  wp_register_style('libraries-child-global', get_template_directory_uri() . '/css/build/minified/libraries-child-global.css', array(), '20140321');
   wp_register_style('libraries-child-global', get_stylesheet_directory_uri() . '/css/build/minified/libraries-child-global.css', array(), '20140321');
   wp_enqueue_style('libraries-child-global');
 }
+
+add_action( 'after_setup_theme', 'remove_child_theme_support');
 
 function remove_child_theme_support() {
   remove_theme_support('custom-background');
